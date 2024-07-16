41d217b7dac3946fc2ec98e20d047a2b6a47d718
Ammar Rexhaj <167920633+bandyburrito@users.noreply.github.com>
2024-07-16 01:06:38 -0700

    Add files via upload
    ushtrime

---
+++ ch3ushtrim.py
@@ -0,0 +1,8 @@
+salary = [340, 210, 450, 600, 230, 500, 550, 300]
+shuma = 0
+for paga in salary:
+    shuma = shuma + paga
+print(f"Shuma e pagave eshte: {shuma}")
+
+pagamesatare = shuma / len(salary)
+print(pagamesatare)
